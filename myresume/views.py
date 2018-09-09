b0b371bc01b9b5e77aa16e3054b59a333543b020
sandy <sandycool523@gmail.com>
2018-09-09 12:25:04 +0530

    second commit

---
+++ myresume/views.py
@@ -0,0 +1,7 @@
+import requests
+from django.shortcuts import render, render_to_response, redirect
+
+def myresume(request):
+    return_data = {}
+    return render (request, 'index.html', return_data)
+
